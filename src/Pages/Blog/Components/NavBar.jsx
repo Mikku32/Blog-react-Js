e8467f82b34879efab6a60f5a3d44511924463b2
Mikdhad.k <mikdhadk32@gmail.com>
2024-06-21 14:45:31 +0530

    modal

--- src/Pages/Blog/Components/NavBar.jsx
+++ src/Pages/Blog/Components/NavBar.jsx
@@ -1,6 +1,16 @@
 import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/modal";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isOpen, setOpenModal] = useState(false);
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -13,11 +23,54 @@ const NavBar = () => {
           <Button color="primary" variant="ghost">
             Blog
           </Button>
-          <Button color="primary" variant="ghost">
+          <Button
+            color="primary"
+            variant="ghost"
+            onClick={() => setOpenModal(true)}
+          >
             Post
           </Button>
         </div>
       </div>
+      <Modal backdrop="blur" isOpen={isOpen} onOpenChange={setOpenModal}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Add Blog
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat
+                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
+                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
+                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </nav>
   );
 };
